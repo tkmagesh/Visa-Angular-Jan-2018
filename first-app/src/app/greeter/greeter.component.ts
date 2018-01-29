3e1ef0326d56709f5f46c79cbb2bde4af3f78f4a
Magesh <tkmagesh77@gmail.com>
2018-01-29 14:41:05 +0530

    calculator -1 assignment

--- first-app/src/app/greeter/greeter.component.ts
+++ first-app/src/app/greeter/greeter.component.ts
@@ -5,5 +5,9 @@ import { Component } from '@angular/core';
 	templateUrl : 'greeter.component.html'
 })
 export class GreeterComponent{
+	message :string = '[Default Greet Message]'
 
+	onGreetClick(username){
+		this.message = `Hi ${username}, Have a nice day!`;
+	} 
 }
