985502b67e906006faf3ff0f74ec6a64b14c020f
Magesh <tkmagesh77@gmail.com>
2018-01-29 15:09:23 +0530

    calculator-one solution

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GreeterComponent } from './greeter/greeter.component';
+import { CalculatorOneComponent } from './calculator/CalculatorOne.component';
 
 @NgModule({
   declarations: [
     AppComponent
     , GreeterComponent
+    , CalculatorOneComponent
   ],
   imports: [
     BrowserModule
+    , FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CalculatorModel } from './CalculatorModel';
+
+@Component({
+	selector : 'app-calculator-one',
+	templateUrl : 'calculatorOne.component.html'
+})
+export class CalculatorOneComponent{
+	
+	model : CalculatorModel = new CalculatorModel();
+
+}
